a39c7947a8d077c03c1ba41149b536aca2a518aa
eadogan <e.ahmetdogan@gmail.com>
2021-05-09 08:49:04 +0100

    Create WebChat skeleton

---
+++ src/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+// Loading dependency & initizlizing express
+const os = require('os')
+const express = require('express')
+const http = require('http')
+const path = require('path')
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+const app = express()
+
+// Signaling WebRTC
+const socketIO = require('socket.io')
+
+// Define the folder which cointans the CSS and JS for the frontend
+app.use(express.static(publicDirectoryPath))
+
+app.use('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+const server = http.createServer(app)
+const io = socketIO(server)
+
+
+io.on('connection', (server) => {
+    console.log('A new user has connected!')
+})
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`Server is on port ${port}`)
+})
